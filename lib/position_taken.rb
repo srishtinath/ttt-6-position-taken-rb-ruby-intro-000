53a180386935f0049d2aa6c9d8788f8288b00c06
Srishti Nath <srishti.nath@gmail.com>
2020-03-31 04:51:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
-def position_taken?(position)
+def position_taken?(board, index)
   if board[position.to_i-1] != " "
     true
   elsif board[position.to_i-1] != ""
