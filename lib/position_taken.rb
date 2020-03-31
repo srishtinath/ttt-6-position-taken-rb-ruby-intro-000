fad75e18fc208e606f02e4a79d573bfa0444d7dd
Srishti Nath <srishti.nath@gmail.com>
2020-03-31 04:54:10 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
     false
   elsif board[index] != ""
     false
-  elsif board[index] == "X" || board[index] == "O"
+  elsif (board[index] == 'X') || (board[index] == 'O')
     true
   else
     true
